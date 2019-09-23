ceb9df15a45a31fc150bfd4278c42251355997ed
Melvin Lau <lau.melvin@icloud.com>
2019-09-23 15:00:35 +0100

    Completed first 3 user stories

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  let(:card) {described_class.new}
+
+  it "has a balance" do
+    expect(card.balance).to eq 0.00
+  end
+
+  it "allows money to be added to balance" do
+    card.add(1.00)
+    expect(card.balance).to eq 1.00
+  end
+
+  it "has a limit on the balance" do
+    card.add(100.00)
+    expect(card.add(1.00)).to eq "Exceeded card balance limit"
+  end
+
 end
