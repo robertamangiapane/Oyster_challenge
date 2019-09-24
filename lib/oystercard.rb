a93478e4e71fba9748ce598240416284e6e212f3
robertamangiapane <roberta.mangiapane@gmail.com>
2019-09-24 10:56:49 +0100

    Step 10 Refactoring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,31 +2,44 @@ class Oystercard
   attr_accessor :balance
   attr_reader :journey
 
-  DEFAULT_LIMIT = 100.00
+  DEFAULT_LIMIT = 90.00
   MINIMUM_AMOUNT = 2.00
 
-  def initialize
+  def initialize(limit = DEFAULT_LIMIT, min_amount = MINIMUM_AMOUNT)
+    @limit = limit
+    @min_amount = min_amount
     @balance = 0.00
-
   end
 
-  def add(amount)
-    return "Exceeded card balance limit" if overlimit?(amount)
+  def top_up(amount)
+    return 'Exceeded card balance limit' if overlimit?(amount)
+
     @balance += amount
+  end
+
+  def touch_in
+    raise 'Insufficient balance' if insufficient_funds?
+
+    @journey = true
+  end
+
+  def touch_out(min_amount)
+    @journey = false
+    deduct(min_amount)
+  end
+
+  # def calculate(start, finish)
+  #   @balance = (finish - start) * 2
+  # end
+
+  private
+
+  def in_journey?
+    @journey
   end
 
   def deduct(fare)
     @balance -= fare
-  end
-
-  def touch_in
-    fail "Insufficient balance" if insufficient_funds?
-    @journey = true
-  end
-
-  def touch_out(fare)
-    @journey = false
-    deduct(fare)
   end
 
   def overlimit?(amount)
@@ -36,9 +49,4 @@ class Oystercard
   def insufficient_funds?
     @balance <= MINIMUM_AMOUNT
   end
-
-  def calculate(start, finish)
-
-  end
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,73 +2,55 @@ require 'oystercard'
 require 'station'
 
 describe Oystercard do
-
   let(:max_balance) { described_class::DEFAULT_LIMIT }
-
-  let(:card) {described_class.new}
-
-  it "has a balance" do
+  let(:min_amount) { described_class::MINIMUM_AMOUNT }
+  let(:card) { described_class.new }
+  it 'has a balance' do
     expect(card.balance).to eq 0.00
   end
 
-  it "#add allows money to be added to balance" do
-    card.add(1.00)
-    expect(card.balance).to eq 1.00
-  end
-
-  it "has a limit on the balance" do
-    card.add(max_balance)
-    expect(card.add(1.00)).to eq "Exceeded card balance limit"
-  end
-
-
-  describe "#deduct" do
-
-    it "deducts fare from the balance" do
-      card.add(2.00)
-      card.deduct(1.50)
-      expect(card.balance).to eq 0.50
+  describe '#top_up' do
+    it '#top_ip allows money to be added to balance' do
+      card.top_up(1.00)
+      expect(card.balance).to eq 1.00
     end
 
-    it "deducts the fare automatically when the journey has ended" do
-      card.add(10.00)
+    it 'has a limit on the balance' do
+      card.top_up(max_balance)
+      expect(card.top_up(1.00)).to eq 'Exceeded card balance limit'
+    end
+  end
+
+  describe '#touch_in' do
+    it 'checks the minimum amount' do
+      expect { card.touch_in }.to raise_error 'Insufficient balance'
+    end
+    it 'starts a journey' do
+      card.balance = 10.00
+      allow(card).to receive(:in_journey?).and_return(true)
+    end
+  end
+
+  describe '#touch_out' do
+    it 'ends a journey' do
+      card.touch_out(min_amount)
+      allow(card).to receive(:in_journey?).and_return(false)
+    end
+  end
+
+  describe '#deduct' do
+    it 'deducts the fare automatically when the journey has ended' do
+      card.balance = 10.00
       card.touch_in
-      expect(card.touch_out(2.00)).to eq 8.00
+      expect(card.touch_out(min_amount)).to eq 8.00
     end
-
   end
 
-
-
-  describe "#touch_in" do
-
-    it "checks the minimum amount" do
-      expect {card.touch_in}.to raise_error "Insufficient balance"
-    end
-
-    it "starts a journey" do
-      card.add(10.00)
-      expect(card.touch_in).to eq true
-    end
-
-  end
-
-  describe "#touch_out" do
-
-    it "ends a journey" do
-      card.touch_out(2.00)
-      expect(card.journey).to eq false
-    end
-
-  end
-
-  describe "#calculate" do
-
-    it "calculates a fare given start and end locations" do
-      expect(card.calculate(1, 4)).to eq 6.00
-    end
-
-  end
+  # describe '#calculate' do
+  #   it 'calculates a fare given start and end locations' do
+  #     expect { card.calculate(1, 4) }.to change { card.balance }.by(6.00)
+  #   end
+  # end
 
 
 
